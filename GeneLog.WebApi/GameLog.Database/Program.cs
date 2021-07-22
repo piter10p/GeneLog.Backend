53f8834a6252a66fa3f31c26354585b6f18fb138
Piotr Paszko <piotr.paszko.dev@gmail.com>
2021-07-22 23:54:24 +0200

    Create database migration project

---
+++ GeneLog.WebApi/GameLog.Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameLog.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
